e98f40c5aacbe5e32a5082ade73c7cd1eca36e56
Ignacio <naxete7@hotmail.es>
2022-10-03 17:13:02 +0200

    ejercicio 1

---
+++ ejercicio1.js
@@ -0,0 +1,10 @@
+var num1 = 5;
+var num2 = 7;
+
+if (num1 > num2) {
+    console.log("El numero 1 es mayor que el numero 2");
+} else if (num1 < num2) {
+    console.log("el numero 1 es menor que el numero 2");
+} else {
+    console.log("los dos numeros son iguales");
+}
