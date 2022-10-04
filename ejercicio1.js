7e68f5999879a5bb88f6280ea9a71cb58df74064
Ignacio <naxete7@hotmail.es>
2022-10-04 09:42:44 +0200

    añadido enunciado

--- ejercicio1.js
+++ ejercicio1.js
@@ -1,3 +1,8 @@
+//1. Declara 2 variables numéricas (con el valor que desees), indica cual es mayor de
+//los dos. Si son iguales indicalo también. Puedes ir cambiando los valores
+//para comprobar que funciona.
+
+
 var num1 = 5;
 var num2 = 7;
 
